4f4a072946fcf3bd5b516c25479d75aa14eda57e
HenKyubi666 <jhonnino@unisangil.edu.co>
2021-11-20 10:14:26 -0500

    filter-colors-build

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -33,3 +33,45 @@ export const fetchPokemonData = id => {
     }
   })
 }
+
+export const fetchPokemonSpecie = id => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const { data } = await axios.get(`${URL}pokemon-species/${id}`)
+      resolve(data)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+
+export const getInitialPokemons = (initial = 0) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let { data } = await axios.get(`${URL}pokedex/national/`)
+      // console.log("data-", data)
+      const max =
+        initial + 20 >= data.pokemon_entries.length
+          ? null
+          : data.pokemon_entries.length - initial + 20 <= 20
+          ? initial + 20
+          : data.pokemon_entries.length - initial + 20
+      if (max !== null) {
+        // Save data.pokemon_entries in localstorage
+        for (let i = initial; i <= max; i++) {
+          // data.pokemon_entries[i].data = await fetchPokemonData(
+          //   data.pokemon_entries[i].entry_number
+          // )
+          // res.results[i].specie = await fetchPokemonSpecie(
+          //   Number(data.pokemon_entries[i].entry_number)
+          // )
+        }
+      }
+      let res = { results: data.pokemon_entries }
+      console.log("getInitial", res)
+      resolve(res)
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
